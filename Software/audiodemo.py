6a935c5c2508bc6c66ec0cae1392224d51903247
Robert Rattray <rmr3141@gmail.com>
2026-04-03 15:07:42 -0400

    Latest version of audiodemo, works on Pi

--- Software/audiodemo.py
+++ Software/audiodemo.py
@@ -1,62 +1,73 @@
-import pyaudio
-import time
-import collections
-
-import butterworth
-
-FORMAT = pyaudio.paInt16
-CHANNELS = 1
-SAMPLE_RATE = 44100
-BUFFER_LENGTH = 1024
-
-pa = pyaudio.PyAudio()
-
-buffer = collections.deque()
-silence = b"\x00" * (BUFFER_LENGTH * 2)  # initialize with silence (16‑bit audio)
-buffer.append(silence)
-
-myFilter = butterworth.Filter(SAMPLE_RATE)
-
-firstTime = True
-
-def input_callback(in_data, frame_count, time_info, status):
-    global buffer
-    global firstTime
-    if firstTime:
-        print(in_data[:10])
-        firstTime = False
-    buffer.append(myFilter.filter(in_data))
-    # buffer.append(in_data)
-    return (None, pyaudio.paContinue)
-
-def output_callback(in_data, frame_count, time_info, status):
-    global buffer
-    if len(buffer) == 0:
-        return (b"\x00" * (frame_count * 2), pyaudio.paContinue)
-    return (buffer.popleft(), pyaudio.paContinue)
-
-mic_stream = pa.open(
-    format=FORMAT,
-    channels=CHANNELS,
-    rate=SAMPLE_RATE,
-    input=True,
-    input_device_index=12,
-    frames_per_buffer=BUFFER_LENGTH,
-    stream_callback=input_callback
-)
-
-speaker_stream = pa.open(
-    format=FORMAT,
-    channels=CHANNELS,
-    rate=SAMPLE_RATE,
-    output=True,
-    output_device_index=12,
-    frames_per_buffer=BUFFER_LENGTH,
-    stream_callback=output_callback
-)
-
-mic_stream.start_stream()
-speaker_stream.start_stream()
-
-while mic_stream.is_active() and speaker_stream.is_active():
-    time.sleep(0.1)
+import pyaudio                                                                  
+import time                                                                     
+import collections                                                              
+import os                                                                       
+import sys                                                                      
+                                                                                
+import butterworth                                                              
+                                                                                
+FORMAT = pyaudio.paInt16                                                        
+CHANNELS = 1                                                                    
+SAMPLE_RATE = 44100                                                             
+if (os.uname().nodename == "pi"):                                               
+    SAMPLE_RATE = 48000                                                         
+BUFFER_LENGTH = 1024                                                            
+                                                                                
+pa = pyaudio.PyAudio()                                                          
+                                                                                
+buffer = collections.deque()                                                    
+silence = b"\x00" * (BUFFER_LENGTH * 2)  # initialize with silence (16‑bit audi)
+buffer.append(silence)                                                          
+                                                                                
+LOW_FREQ = 60                                                                   
+HIGH_FREQ = 220                                                                 
+if (len(sys.argv) == 3):                                                        
+    LOW_FREQ = int(sys.argv[1])                                                 
+    HIGH_FREQ = int(sys.argv[2])                                                
+                                                                                
+myFilter = butterworth.Filter(SAMPLE_RATE, LOW_FREQ, HIGH_FREQ)                 
+pa = pyaudio.PyAudio()                                                          
+                                                                                
+firstTime = True                                                                
+                                                                                
+def input_callback(in_data, frame_count, time_info, status):                    
+    global buffer                                                               
+    global firstTime                                                            
+    if firstTime:                                                               
+        print(in_data[:10])                                                     
+        firstTime = False                                                       
+    # buffer.append(in_data)                                                    
+    print(in_data[:40])                                                         
+    print(frame_count)                                                          
+    buffer.append(myFilter.filter(in_data))                                     
+    return (None, pyaudio.paContinue)                                           
+                                                                                
+def output_callback(in_data, frame_count, time_info, status):                   
+    global buffer                                                               
+    if len(buffer) == 0:                                                        
+        return (b"\x00" * (frame_count * 2), pyaudio.paContinue)                
+    return (buffer.popleft(), pyaudio.paContinue)                               
+                                                                                
+mic_stream = pa.open(                                                           
+    format=FORMAT,                                                              
+    channels=CHANNELS,                                                          
+    rate=SAMPLE_RATE,                                                           
+    input=True,                                                                 
+    frames_per_buffer=BUFFER_LENGTH,                                            
+    stream_callback=input_callback                                              
+)                                                                               
+                                                                                
+speaker_stream = pa.open(                                                       
+    format=FORMAT,                                                              
+    channels=CHANNELS,                                                          
+    rate=SAMPLE_RATE,                                                           
+    output=True,                                                                
+    frames_per_buffer=BUFFER_LENGTH,                                            
+    stream_callback=output_callback                                             
+)                                                                               
+                                                                                
+mic_stream.start_stream()                                                       
+speaker_stream.start_stream()                                                   
+                                                                                
+while mic_stream.is_active() and speaker_stream.is_active():                    
+    time.sleep(0.1) 
